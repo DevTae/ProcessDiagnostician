3c5425105e4e7d3ad86d9fe31d2a466de249f322
ahdelron <kthday107@gmail.com>
2019-12-21 22:52:46 +0900

    검사 대상 파일 업로드까지 성공

--- vtapi_get.py
+++ vtapi_get.py
@@ -1,5 +1,11 @@
+'''
+VirusTotal API
+vtapi_get.py
+'''
+
 import requests
 import sys
+import json
 
 url = 'https://www.virustotal.com/vtapi/v2/file/report'
 # My api key # Don't use this key on anything program else.
@@ -7,4 +13,12 @@ api_key = '0f839cbc51dbca7de87e34c963743592b03b47d4230a0c96316f89ba512e2f57'
 resource = sys.argv[1]
 params = {'apikey': api_key, 'resource': resource}
 response = requests.get(url, params=params)
-print(response.json())
+
+response_dict = json.loads(response.text)
+scans = response_dict['scans']
+keys = scans.keys()
+
+for key in keys:
+	print "%-20s : %s" % (key, scans[key]['result'])
+
+#print(response.json())
